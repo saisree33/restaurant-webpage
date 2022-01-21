a032fc8c743dbf417e9921922712f14d3d0acfda
saisree33 <saisreereddymadadi33@gmail.com>
2022-01-21 20:13:05 +0530

    mongo-connection

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb://localhost:27017/admin"
+    "mongodb+srv://assignment:assignment@cluster0.f8kbc.mongodb.net/test"
 );
 
 app.post("/insert", async (req, res) => {
